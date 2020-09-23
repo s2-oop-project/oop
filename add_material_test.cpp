9bcf1d6a45a0e5b8525845e1c61d40d4dff50772
Truth-Raven <65074319+Truth-Raven@users.noreply.github.com>
2020-09-23 15:01:23 +0930

    Update add_material_test.cpp

--- add_material_test.cpp
+++ add_material_test.cpp
@@ -1,10 +1,8 @@
 //This is the test of add material
-
 #include<iostream>
 #include<string>
 #include<vector>
 #include"material.h"
-
 
 extern vector<Ebook> add_ebook(vector<Ebook>);
 extern vector<book> add_book(vector<book>);
@@ -14,14 +12,23 @@ using namespace std;
 
 int main(){
     vector<Ebook> Ebook_Collection;
+    
     vector<book> book_Collection;
+    
     vector<DVD>  DVD_Collection;
+    
     Ebook_Collection=add_ebook(Ebook_Collection);
+    
     book_Collection=add_book(book_Collection);
+    
     DVD_Collection=add_DVD(DVD_Collection);
+    
     Ebook_Collection=add_ebook(Ebook_Collection);
+    
     book_Collection=add_book(book_Collection);
+    
     DVD_Collection=add_DVD(DVD_Collection);
+    
     for(int i=0;i<2;i++){
         cout<<Ebook_Collection[i].get_material_name()<<" ";
         cout<<book_Collection[i].get_material_name()<<" ";
